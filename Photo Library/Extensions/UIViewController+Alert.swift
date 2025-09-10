28f13ececde82d72f45833884a20a14b5b10c0cf
Aleksey Kemezhuk <aleksey.kemezhuk@gmail.com>
2025-09-10 11:28:30 +0200

    add showTextFieldAlert method

--- Photo Library/Extensions/UIViewController+Alert.swift
+++ Photo Library/Extensions/UIViewController+Alert.swift
@@ -1,13 +1,36 @@
 import UIKit
 
 extension UIViewController {
-    func showAlert(
-        title: String,
-        message: String,
-        actions: [UIAlertAction] = [UIAlertAction(title: "OK", style: .default, handler: nil)]
+    func showAlert(title: String, message: String,
+                   actions: [UIAlertAction] = [UIAlertAction(title: "OK", style: .default, handler: nil)]
     ) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         actions.forEach { alert.addAction($0) }
         present(alert, animated: true)
     }
+    
+    func showTextFieldAlert(
+            title: String,
+            message: String,
+            placeholder: String? = nil,
+            isSecure: Bool = false,
+            confirmTitle: String = "OK",
+            cancelTitle: String = "Cancel",
+            onConfirm: @escaping (String) -> Void
+        ) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addTextField { textField in
+                textField.placeholder = placeholder
+                textField.isSecureTextEntry = isSecure
+            }
+            let confirmAction = UIAlertAction(title: confirmTitle, style: .default) { _ in
+                if let text = alert.textFields?.first?.text, !text.isEmpty {
+                    onConfirm(text)
+                }
+            }
+            let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel)
+            alert.addAction(cancelAction)
+            alert.addAction(confirmAction)
+            present(alert, animated: true)
+        }
 }
